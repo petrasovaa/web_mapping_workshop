0d9a4b1275834c657e69c210614ed7e7d27247b7
Anna Petrasova <kratochanna@gmail.com>
2016-05-02 15:23:12 -0400

    fix

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -31,7 +31,7 @@ featureLayer.on('ready', function(){
      })
     map.fitBounds(featureLayer.getBounds());
 })
-featureLayer.on('ready'. function(){
+featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
        layer.bindPopup('Welcome to' + layer.feature.properties.name);
 })
