06cb23adcec453b6136d2e08bb7b7641c1179e42
Anna Petrasova <kratochanna@gmail.com>
2016-05-02 15:25:15 -0400

    missing space

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -33,7 +33,7 @@ featureLayer.on('ready', function(){
 })
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-       layer.bindPopup('Welcome to' + layer.feature.properties.name);
+       layer.bindPopup('Welcome to ' + layer.feature.properties.name);
 })
 })
 
