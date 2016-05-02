5a0f576c9da01bca459b7c3f4f1405b75fdef26f
Anna Petrasova <kratochanna@gmail.com>
2016-05-02 17:37:10 -0400

    current location

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -116,6 +116,12 @@ function getDirections(from, to){
                 "stroke-width": 8
             }
         })
+    $('#directions').fadeIn(400, function(){
+        var summary = data.trip.summary
+        $('summary').empty();
+        $('#distance').text((Math.round(summary.length * 100) / 100) + ' ' + data.trip.units);
+        $('#time').text((Math.round(summary.time, length / 60 * 100) + ' min');
+    })
     })
 }
 
