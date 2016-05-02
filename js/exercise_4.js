316aabc8d2b4e21d8fbf19f9227db65bdb4aa745
Anna Petrasova <kratochanna@gmail.com>
2016-05-02 15:22:32 -0400

    add popup

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -31,4 +31,9 @@ featureLayer.on('ready', function(){
      })
     map.fitBounds(featureLayer.getBounds());
 })
+featureLayer.on('ready'. function(){
+  this.eachLayer(function(layer){
+       layer.bindPopup('Welcome to' + layer.feature.properties.name);
+})
+})
 
